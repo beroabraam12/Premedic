2307e9ba0e3bc228ce5223c3e741c4eb4ebc4f60
beroabraam12 <ebraamboshra7@gmail.com>
2020-06-03 16:10:41 +0200

    edit focus Node for search bar

--- lib/pages/pharmacies.dart
+++ lib/pages/pharmacies.dart
@@ -29,129 +29,132 @@ class _PharmaciesPageState extends State<PharmaciesPage> {
     var mediaquery = MediaQuery.of(context);
     return Scaffold(
         resizeToAvoidBottomPadding: false,
-        body: SingleChildScrollView(
-          child: Container(
-            width: mediaquery.size.width,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: (mediaquery.size.height - mediaquery.padding.top) *
-                        0.07,
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left:
-                            (mediaquery.size.width - mediaquery.padding.left) *
-                                0.03,
-                      ),
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: SingleChildScrollView(
+            child: Container(
+              width: mediaquery.size.width,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: (mediaquery.size.height - mediaquery.padding.top) *
+                          0.07,
                     ),
-                    IconButton(
-                        icon: Icon(
-                          Icons.arrow_back,
-                          size: Theme.of(context).iconTheme.size,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: (mediaquery.size.width -
+                                  mediaquery.padding.left) *
+                              0.03,
                         ),
-                        onPressed: () {
-                          Navigator.pop(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomePage()));
-                        })
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: (mediaquery.size.height - mediaquery.padding.top) *
-                        0.03,
+                      ),
+                      IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: Theme.of(context).iconTheme.size,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                          })
+                    ],
                   ),
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      'Pharmacies',
-                      style: Theme.of(context).textTheme.headline6,
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: (mediaquery.size.height - mediaquery.padding.top) *
+                          0.03,
                     ),
-                    Padding(
-                        padding: EdgeInsets.only(
-                      top: (mediaquery.size.height - mediaquery.padding.top) *
-                          0.03,
-                    )),
-                    DropdownPharmacy(),
-                    Padding(
-                        padding: EdgeInsets.only(
-                      top: (mediaquery.size.height - mediaquery.padding.top) *
-                          0.03,
-                    )),
-                    SearchPharmacy(),
-                    Padding(
-                        padding: EdgeInsets.only(
-                      top: (mediaquery.size.height - mediaquery.padding.top) *
-                          0.01,
-                    )),
-                    Container(
-                      width: (mediaquery.size.width),
-                      height: mediaquery.size.height,
-                      child: ListView.builder(
-                          physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: pharmacyModelCardList.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                              width: (mediaquery.size.width),
-                              height: mediaquery.size.height * 0.20,
-                              padding: EdgeInsets.symmetric(
-                                vertical: (mediaquery.size.height -
-                                        mediaquery.padding.vertical) *
-                                    0.01,
-                                horizontal: (mediaquery.size.width -
-                                        mediaquery.padding.horizontal) *
-                                    0.04,
-                              ),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(15)),
-                                  image: DecorationImage(
-                                    colorFilter: ColorFilter.mode(
-                                        Colors.black.withOpacity(0.6),
-                                        BlendMode.dstATop),
-                                    image:
-                                        AssetImage("assets/background-01.png"),
-                                    fit: BoxFit.fill,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'Pharmacies',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(
+                        top: (mediaquery.size.height - mediaquery.padding.top) *
+                            0.03,
+                      )),
+                      DropdownPharmacy(),
+                      Padding(
+                          padding: EdgeInsets.only(
+                        top: (mediaquery.size.height - mediaquery.padding.top) *
+                            0.03,
+                      )),
+                      SearchPharmacy(),
+                      
+                      Container(
+                        width: (mediaquery.size.width),
+                        height: mediaquery.size.height,
+                        child: ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: pharmacyModelCardList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                width: (mediaquery.size.width),
+                                height: mediaquery.size.height * 0.20,
+                                padding: EdgeInsets.symmetric(
+                                  vertical: (mediaquery.size.height -
+                                          mediaquery.padding.vertical) *
+                                      0.01,
+                                  horizontal: (mediaquery.size.width -
+                                          mediaquery.padding.horizontal) *
+                                      0.04,
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15)),
+                                    image: DecorationImage(
+                                      colorFilter: ColorFilter.mode(
+                                          Colors.black.withOpacity(0.6),
+                                          BlendMode.dstATop),
+                                      image: AssetImage(
+                                          "assets/background-01.png"),
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        pharmacyModelCardList[index]
+                                            .pharmacyName,
+                                        style:
+                                            Theme.of(context).textTheme.caption,
+                                      ),
+                                      Padding(
+                                          padding: EdgeInsets.only(
+                                              top: (mediaquery.size.height -
+                                                      mediaquery.padding.top) *
+                                                  0.02)),
+                                      CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                            pharmacyModelCardList[index]
+                                                .pharmacyCircleImage),
+                                        radius: 30,
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(
-                                      pharmacyModelCardList[index].pharmacyName,
-                                      style:
-                                          Theme.of(context).textTheme.caption,
-                                    ),
-                                    Padding(
-                                        padding: EdgeInsets.only(
-                                            top: (mediaquery.size.height -
-                                                    mediaquery.padding.top) *
-                                                0.02)),
-                                    CircleAvatar(
-                                      backgroundImage: NetworkImage(
-                                          pharmacyModelCardList[index]
-                                              .pharmacyCircleImage),
-                                      radius: 30,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          }),
-                    )
-                  ],
-                )
-              ],
+                              );
+                            }),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
