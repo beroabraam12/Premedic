ade46217926ca43c9dde7522a06f54fa563f90af
beroabraam12 <ebraamboshra7@gmail.com>
2020-06-03 18:07:31 +0200

    FIX hospital drop down

--- lib/pages/blogs.dart
+++ lib/pages/blogs.dart
@@ -17,76 +17,80 @@ class _BlogsPageState extends State<BlogsPage> {
     var mediaquery = MediaQuery.of(context);
     return Scaffold(
         resizeToAvoidBottomPadding: false,
-        body: SingleChildScrollView(
-          child: Container(
-            width: mediaquery.size.width,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: (mediaquery.size.height - mediaquery.padding.top) *
-                        0.07,
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left:
-                            (mediaquery.size.width - mediaquery.padding.left) *
-                                0.03,
-                      ),
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: SingleChildScrollView(
+            child: Container(
+              width: mediaquery.size.width,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: (mediaquery.size.height - mediaquery.padding.top) *
+                          0.07,
                     ),
-                    IconButton(
-                        icon: Icon(
-                          Icons.arrow_back,
-                          size: Theme.of(context).iconTheme.size,
-                        ),
-                        onPressed: () {
-                          Navigator.pop(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomePage()));
-                        })
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: (mediaquery.size.height - mediaquery.padding.top) *
-                        0.03,
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Blogs',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    Padding(
+                  Row(
+                    children: <Widget>[
+                      Padding(
                         padding: EdgeInsets.only(
+                          left: (mediaquery.size.width -
+                                  mediaquery.padding.left) *
+                              0.03,
+                        ),
+                      ),
+                      IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: Theme.of(context).iconTheme.size,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                          })
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
                       top: (mediaquery.size.height - mediaquery.padding.top) *
                           0.03,
-                    )),
-                    SearchBlogs(),
-                  ],
-                ),
-                Container(
-                  width: mediaquery.size.width,
-                  height: mediaquery.size.height,
-                  child: GestureDetector(
-                    child: BlogCard(),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => BlogDetailsPage()),
-                      );
-                    },
+                    ),
                   ),
-                )
-              ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Blogs',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(
+                        top: (mediaquery.size.height - mediaquery.padding.top) *
+                            0.03,
+                      )),
+                      SearchBlogs(),
+                    ],
+                  ),
+                  Container(
+                    width: mediaquery.size.width,
+                    child: GestureDetector(
+                      child: BlogCard(),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BlogDetailsPage()),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));
--- lib/pages/hospital_clinc.dart
+++ lib/pages/hospital_clinc.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:premedic/components/Pharmacies/dropdown_list.dart';
 
 import '../components/Hospital_Clinc/filter.dart';
-import '../components/Hospital_Clinc/dropdownlist.dart';
 import '../components/Hospital_Clinc/search.dart';
 import '../components/Hospital_Clinc/hosptial_clinc_card.dart';
 import '../pages/home.dart';
@@ -19,77 +19,83 @@ class _HospitalsClinicsPageState extends State<HospitalsClinicsPage> {
 
     return Scaffold(
         resizeToAvoidBottomPadding: false,
-        body: SingleChildScrollView(
-          child: Container(
-            width: mediaquery.size.width,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: (mediaquery.size.height - mediaquery.padding.top) *
-                        0.07,
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left:
-                            (mediaquery.size.width - mediaquery.padding.left) *
-                                0.03,
-                      ),
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: SingleChildScrollView(
+            child: Container(
+              width: mediaquery.size.width,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: (mediaquery.size.height - mediaquery.padding.top) *
+                          0.07,
                     ),
-                    IconButton(
-                        icon: Icon(
-                          Icons.arrow_back,
-                          size: Theme.of(context).iconTheme.size,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: (mediaquery.size.width -
+                                  mediaquery.padding.left) *
+                              0.03,
                         ),
-                        onPressed: () {
-                          Navigator.pop(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomePage()));
-                        })
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: (mediaquery.size.height - mediaquery.padding.top) *
-                        0.03,
+                      ),
+                      IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: Theme.of(context).iconTheme.size,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                          })
+                    ],
                   ),
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      'Hospitals & Clinics',
-                      style: Theme.of(context).textTheme.headline6,
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: (mediaquery.size.height - mediaquery.padding.top) *
+                          0.03,
                     ),
-                    Padding(
-                        padding: EdgeInsets.only(
-                      top: (mediaquery.size.height - mediaquery.padding.top) *
-                          0.03,
-                    )),
-                    Dropdown(),
-                    Padding(
-                        padding: EdgeInsets.only(
-                      top: (mediaquery.size.height - mediaquery.padding.top) *
-                          0.03,
-                    )),
-                    FilterRow(),
-                    Padding(
-                      padding: EdgeInsets.only(
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'Hospitals & Clinics',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(
                         top: (mediaquery.size.height - mediaquery.padding.top) *
                             0.03,
+                      )),
+                      DropdownPharmacy(),
+                      Padding(
+                          padding: EdgeInsets.only(
+                        top: (mediaquery.size.height - mediaquery.padding.top) *
+                            0.03,
+                      )),
+                      FilterRow(),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: (mediaquery.size.height -
+                                  mediaquery.padding.top) *
+                              0.03,
+                        ),
                       ),
-                    ),
-                    Search(),
-                    Container(
-                      width: mediaquery.size.width * 0.9,
-                      child: HospitalClincCards(),
-                    )
-                  ],
-                )
-              ],
+                      Search(),
+                      Container(
+                        width: mediaquery.size.width * 0.9,
+                        child: HospitalClincCards(),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
